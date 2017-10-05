e092baf8ed56b9a958afdc6454873d7a7f7d115d
David <davidxkang@gmail.com>
2017-10-05 14:29:21 -0700

    connect amazon to app

---
+++ client/src/component/AmazonDRS/amazonLogin.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Amazon extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+
+  render() {
+    return(
+      <div>
+        <div id="amazon-root"></div>
+
+        <script type="text/javascript">
+          {
+            window.onAmazonLoginReady = function() {
+              amazon.Login.setClientId('YOUR-CLIENT-ID');
+            }
+            (function(d) {
+              var a = d.createElement('script'); a.type = 'text/javascript';
+              a.async = true; a.id = 'amazon-login-sdk';
+              a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+              d.getElementById('amazon-root').appendChild(a);
+            })(document)
+            }
+          }  
+        </script>
+
+        <a href id="LoginWithAmazon">
+        <img border="0" alt="Login with Amazon"
+        src="http://g-ecx.images-amazon.com/images/G/01/lwa/btnLWA_gry_156x132.png"
+        width="156" height="32" />
+        </a>
+
+
+      </div>  
+    )
+  }
+}
+
+export default connect(null)(Amazon);
