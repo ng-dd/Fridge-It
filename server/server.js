880034d6e252e770c565f262b6982f9eb097b052
David <davidxkang@gmail.com>
2017-10-05 17:02:12 -0700

    eb extensions added

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middlewares
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
+app.use(parser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
 
